71eafa7b2ac40da33e5cfb6ce8ca77a57a83484b
David Ames <readytowin2298@gmail.com>
2021-05-25 02:25:14 -0500

    unroll done and tested

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,34 @@
 function unroll(squareArray) {
+    if(!squareArray || !squareArray.length) {return undefined}
+    for(let sqare of squareArray){
+        if(sqare.length !== squareArray.length){
+            return undefined;
+        }
+    };
+    const len = squareArray.length;
+    let res = [];
+    while(squareArray.length > 0){
+        // spread the first array in order
+        res.push(...squareArray.shift())
+        if(squareArray.length === 0){break}
 
+        // grab the last element of all arrays
+        squareArray.forEach((arr) => {
+            res.push(arr.pop())
+        })
+        if(squareArray.length === 0){break}
+
+        // get the bottom array revered
+        res.push(...squareArray.pop().reverse());
+        if(squareArray.length === 0){break}
+
+        // get first element of each array, from last to first
+        res.push(...squareArray.map((arr) => arr.shift()).reverse());
+    }
+    
+
+
+    return res;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -7,3 +7,49 @@ describe("#unroll", function () {
   });
 
 });
+
+
+describe("Test unroll functionality", function () {
+  const grid = [
+    [0, 1, 2],
+    [7, 8, 3],
+    [6, 5, 4]
+  ]
+  const nums = unroll(grid)
+  test("Left", function () {
+    expect(nums[2]).toBe(2)
+  })
+  test("Down", function() {
+    expect(nums[4]).toBe(4)
+  })
+  test("right", function () {
+    expect(nums[5]).toBe(5)
+  })
+  test("up", function () {
+    expect(nums[7]).toBe(7)
+  })
+  test("middle", function () {
+    expect(nums[8]).toBe(8)
+  })
+  test("multiple loops work", function () {
+    const grid = [
+      [1, 2, 3, 4, 5],
+      [16, 17, 18, 19, 6],
+      [15, 24, 25, 20, 7],
+      [14, 23, 22, 21, 8],
+      [13, 12, 11, 10, 9]
+    ];
+    const nums = unroll(grid);
+    expect(nums[23]).toBe(24)
+  })
+});
+
+describe("Returns undefined on invalid input", function () {
+  test("no array", function () {
+    expect(unroll([])).toBeUndefined();
+  })
+  test("not a square array", function () {
+    expect(unroll[[1, 2], [1]]).toBeUndefined();
+  })
+})
+
